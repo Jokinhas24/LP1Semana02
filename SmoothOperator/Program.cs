5c4e49fcd98fbb69d6c60ca5ae6f943f71bb0d12
João Nogueira <joaonogueira.art@gmail.com>
2024-02-28 11:44:36 +0000

    Added asking a Integer then increment/decrese set intereger

---
+++ SmoothOperator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SmoothOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insert a positive integer: ");
+            string n = Console.ReadLine();
+            byte numb = byte.Parse(n);
+
+            Console.WriteLine(--numb);
+            Console.WriteLine(++numb);
+        }
+    }
+}
