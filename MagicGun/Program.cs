a88ea334573031495b4f641a297821c6bdb48d49
João Nogueira <joaonogueira.art@gmail.com>
2024-03-01 16:32:17 +0000

    Added shooting loop with respective messages

--- MagicGun/Program.cs
+++ MagicGun/Program.cs
@@ -6,11 +6,32 @@ namespace MagicGun
     {
         static void Main(string[] args)
         {
-            Console.Write("How many Shoots do you want to shot? ");
+            Console.Write("Number of shots: ");
             string numb = Console.ReadLine();
 
             int n = int.Parse(numb);
-            
+
+            for(int s = 1; s <= n; s++)
+            {
+                if (s % 3 == 0 && s % 5 == 0)
+                {
+                    Console.WriteLine("Magic Fire! Electric Fire!");
+                    continue;
+                }
+                else if (s % 3 == 0)
+                {
+                     Console.WriteLine("Magic Fire!");
+                }
+                else if (s % 5 == 0)
+                {
+                     Console.WriteLine("Electric Fire!");
+                }
+                else
+                {
+                    Console.WriteLine("Normal Fire :/");
+                }
+
+            }
 
         }
     }
