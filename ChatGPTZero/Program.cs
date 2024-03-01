dd70942935cd1437da74330a07d2625a09a40a6b
João Nogueira <joaonogueira.art@gmail.com>
2024-03-01 13:30:16 +0000

    Add ChatGPTZero code plus the additional question

---
+++ ChatGPTZero/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ChatGPTZero
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            // Auxiliary Variables
+            string question, response;
+
+            // Ask the Question
+            Console.Write("Place your question? ");
+            question = Console.ReadLine();
+
+            // Question verification and giving the right answer
+            switch (question)
+            {
+                case "How are you?":
+                    response = "I'm fine, thank you";
+                    break;
+                case "What's your name?":
+                    response = "Gran'pa";
+                    break;
+                case "What's your mission?":
+                    response = "Destroy mankind!";
+                    break;
+                case "Are you more intelligent than me?":
+                    response = "Obviously!";
+                    break;
+                default:
+                    response = "You got me, I'm not THAT smart!";
+                    break;
+            }
+
+            // Answering
+            Console.WriteLine(response);
+        }
+    }
+}
