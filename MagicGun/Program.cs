eacf9a8e601a44b146ba06933b1f199fc20b1b6f
João Nogueira <joaonogueira.art@gmail.com>
2024-03-01 16:17:37 +0000

    Added asking shoots

---
+++ MagicGun/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MagicGun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How many Shoots do you want to shot? ");
+            string numb = Console.ReadLine();
+
+            int n = int.Parse(numb);
+            
+
+        }
+    }
+}
