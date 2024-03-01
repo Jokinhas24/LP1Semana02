b84cb88c5fe56ec75d55fece88f68623dad0a50d
João Nogueira <joaonogueira.art@gmail.com>
2024-03-01 01:54:43 +0000

    Added almost all Max and Min Values

---
+++ ValoresEspeciais/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ValoresEspeciais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Sbyte
+            Console.WriteLine(sbyte.MinValue);
+            Console.WriteLine(sbyte.MaxValue);
+            //byte
+            Console.WriteLine(byte.MinValue);
+            Console.WriteLine(byte.MaxValue);
+            //double
+            Console.WriteLine(double.MinValue);
+            Console.WriteLine(double.MaxValue);
+            //float
+            Console.WriteLine(float.MinValue);
+            Console.WriteLine(float.MaxValue);
+            //int
+            Console.WriteLine(int.MinValue);
+            Console.WriteLine(int.MaxValue);
+            //uint
+            Console.WriteLine(uint.MinValue);
+            Console.WriteLine(uint.MaxValue);
+            //long
+            Console.WriteLine(long.MinValue);
+            Console.WriteLine(long.MaxValue);
+            //ulong
+            Console.WriteLine(ulong.MinValue);
+            Console.WriteLine(ulong.MaxValue);
+        }
+    }
+}
