410a7d080bec0229bb6b67d1a9d8d8a31d002150
João Nogueira <joaonogueira.art@gmail.com>
2024-03-01 01:22:57 +0000

    Added asking for height and radius, and possible formula

---
+++ Cilindro/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Cilindro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insert and height for a Cylinder: ");
+            string h = Console.ReadLine();
+            float height = float.Parse(h);
+
+            Console.Write("Now Insert a radius for the Base: ");
+            string r = Console.ReadLine();
+            float radius = float.Parse(r);
+
+            //PI R ^2 A
+
+            Console.WriteLine(MathF.PI * MathF.Low(r, 2) * h);
+
+            //2 PI R
+        }
+    }
+}
