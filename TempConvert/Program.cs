3d3ad6c8b71cca3e289a26b31fe213296b52c054
[João Nogueira] <146846348+Jokinhas24@users.noreply.github.com>
2026-02-26 16:23:11 +0000

    Add ogirin conversion and display

--- TempConvert/Program.cs
+++ TempConvert/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Data;
 using System.Net;
+using System.Net.Sockets;
 using System.Security.Cryptography;
 
 namespace TempConvert
@@ -9,10 +11,22 @@ namespace TempConvert
         private static void Main(string[] args)
         {
             Console.WriteLine("Insert a temperature:");
-            int t = int.Parse(Console.ReadLine());
+            float t = float.Parse(Console.ReadLine());
 
             Console.WriteLine("and is it 'C' (for Celsius) or F (for Fahrenheit)");
             string og = Console.ReadLine();
+
+            switch (og)
+            {
+                case "F":
+                double tf = t * 1.8 + 32;
+                Console.WriteLine("You temperature in the other origin is: " + tf.ToString("F3"));
+                    break;
+                case "C":
+                double tc = (t - 32)/1.8;
+                Console.WriteLine("You temperature in the other origin is: " + tc.ToString("F3"));
+                    break;
+            }
         }
     }
 }
