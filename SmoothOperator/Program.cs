05279a14c76f7e36bdfa4d44b0fa3382c813c9c7
[João Nogueira] <146846348+Jokinhas24@users.noreply.github.com>
2026-02-26 15:16:20 +0000

    Add ask number and increment/decrement

---
+++ SmoothOperator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SmoothOperator
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Write a integer:");
+            sbyte n = sbyte.Parse(Console.ReadLine());
+    
+            Console.WriteLine(-- n);
+            Console.WriteLine(++ n);
+        }
+    }
+}
