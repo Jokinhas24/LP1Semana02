ce56497423227efc4892cc5cfea87183beecdaf0
[João Nogueira] <146846348+Jokinhas24@users.noreply.github.com>
2026-02-26 15:23:56 +0000

    Add heigh and radius

---
+++ Cilindro/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Cilindro
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Whats the cylinder Heigh?");
+            double a = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Whats the cylinder radius?");
+            double r = double.Parse(Console.ReadLine());
+        }
+    }
+}
