ffba5e93f6bfcf6bfd35308046714c7e082e5038
[João Nogueira] <146846348+Jokinhas24@users.noreply.github.com>
2026-02-26 15:33:51 +0000

    Add volume calculations

--- Cilindro/Program.cs
+++ Cilindro/Program.cs
@@ -11,6 +11,11 @@ namespace Cilindro
 
             Console.WriteLine("Whats the cylinder radius?");
             double r = double.Parse(Console.ReadLine());
+
+            double v = Math.PI * Math.Pow(r, 2) * a;
+
+            Console.WriteLine("Volume of the cylinder is:", v);
+
         }
     }
 }
