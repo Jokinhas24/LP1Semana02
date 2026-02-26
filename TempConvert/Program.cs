bab5dfed3d1cbd48ed83afca220021e1e7c73f86
[João Nogueira] <146846348+Jokinhas24@users.noreply.github.com>
2026-02-26 16:11:25 +0000

    Ask temperature and origin

---
+++ TempConvert/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net;
+using System.Security.Cryptography;
+
+namespace TempConvert
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Insert a temperature:");
+            int t = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("and is it 'C' (for Celsius) or F (for Fahrenheit)");
+            string og = Console.ReadLine();
+        }
+    }
+}
